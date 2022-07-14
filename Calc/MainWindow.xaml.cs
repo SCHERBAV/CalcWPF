857d336f6d16538d35188314bc460a5792e6fec5
Vadim Scherba <VSBr1ck@mail.ru>
2022-07-14 23:33:24 +0300

    No name commit 0

--- Calc/MainWindow.xaml.cs
+++ Calc/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calc
 {
@@ -20,6 +8,51 @@ namespace Calc
     /// </summary>
     public partial class MainWindow : Window
     {
+        /*Select argument which handling at moment*/
+        private enum Arg
+        {
+            First = 1,
+            Second
+        }
+
+        /*What is the real arhument*/
+        private enum Dot 
+        {
+            First = 1,
+            Second,
+            None
+        }
+
+        private double arg1, arg2, res;
+        //private bool degFlag;
+        private Arg arg = Arg.First;    //Firstly working with the first argument
+        private Dot dot = Dot.None;
+
+        private void but_del_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void but_result_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void but_clr_Click(object sender, RoutedEventArgs e)
+        {
+            /*Reset all flags and parameters enum*/
+        }
+
+        private void but_Click(object sender, RoutedEventArgs e)
+        {
+            if(Arg.First == arg)
+            {
+                /*Filling in the first argument*/
+                tb_arg1.Text += ((Button)sender).Content.ToString();
+            }
+            else tb_arg2.Text += ((Button)sender).Content.ToString();
+        }
+
         public MainWindow()
         {
             InitializeComponent();
